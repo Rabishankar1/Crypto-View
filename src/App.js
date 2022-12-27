a94ad72d279ebd1b224f009359f9d5bf37eafa68
Rabishankar Panigrahi <rabishankar.panigrahi14@gmail.com>
2022-12-27 13:58:58 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,177 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import { CircularProgress } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+
 
 function App() {
+  const [coinsData, setCoinsData] = useState([]);
+  const [markets, setMarkets] = useState({});
+  const [sort, setSort] = useState({ marketCap: '', priceChange1d: '', change: '' });
+  const [pageLoading, setPageLoading] = useState(false);
+
+  useEffect(() => {
+
+    async function fetchData() {
+      setPageLoading(true);
+      let response = await axios.get('https://api.coinstats.app/public/v1/coins');
+      console.log(response.data);
+      setCoinsData(response.data.coins)
+      setPageLoading(false);
+    }
+    fetchData();
+
+  }, [])
+
+
+  const handleClick = async (event, coinId) => {
+    if (!markets[coinId]) {
+      let response = await axios.get(`https://api.coinstats.app/public/v1/markets?coinId=${coinId}`);
+      setMarkets({ ...markets, [coinId]: response.data });
+    }
+  }
+
+  const handleMarketSort = () => {
+    const data = [...coinsData].sort((a, b) => {
+      if (sort.marketCap !== 'descending') {
+        setSort({ marketCap: 'descending', priceChange1d: '', price: '' });
+        return (b.marketCap - a.marketCap)
+      }
+      else {
+        setSort({ marketCap: 'ascending', priceChange1d: '', price: '' });
+        return (a.marketCap - b.marketCap)
+      }
+    })
+    setCoinsData(data);
+  }
+  const handlePriceSort = () => {
+    const data = [...coinsData].sort((a, b) => {
+      if (sort.price !== 'descending') {
+        setSort({ price: 'descending', marketCap: '', priceChange1d: '' });
+        return (b.price - a.price)
+      }
+      else {
+        setSort({ price: 'ascending', marketCap: '', priceChange1d: '' });
+        return (a.price - b.price)
+      }
+    })
+    setCoinsData(data);
+  }
+
+  const handleChangeSort = () => {
+    const data = [...coinsData].sort((a, b) => {
+      if (sort.priceChange1d !== 'descending') {
+        setSort({ priceChange1d: 'descending', marketCap: '', price: '' });
+        return (b.priceChange1d - a.priceChange1d)
+      }
+      else {
+        setSort({ priceChange1d: 'ascending', marketCap: '', price: '' });
+        return (a.priceChange1d - b.priceChange1d)
+      }
+    })
+    setCoinsData(data);
+  }
+
+
+
+  console.log(markets, 'markets')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='banner'></div>
+      <div className="App">
+      <h1 className='main-heading'>Crypto Markets, Prices and News</h1>
+        {coinsData.length ?
+          (<div>
+            <div className='heading'>
+              <div className="rank heading-item">Rank</div>
+              <div className="name heading-item">Name</div>
+              <div className="price sort heading-item" onClick={() => handlePriceSort()}>
+                Price
+                {sort.price === 'descending' ? <ArrowDropDownIcon /> : sort.price === 'ascending' ? <ArrowDropUpIcon /> : ''}
+              </div>
+              <div className="market-cap sort" onClick={() => handleMarketSort()}>
+                Market cap
+                {sort.marketCap === 'descending' ? <ArrowDropDownIcon /> : sort.marketCap === 'ascending' ? <ArrowDropUpIcon /> : ''}
+              </div>
+              <div className="volume heading-item">Volume</div>
+              <div className="supply heading-item">Supply</div>
+              <div className="change sort heading-item" onClick={() => handleChangeSort()}>
+                % Change(24hr)
+                {sort.priceChange1d === 'descending' ? <ArrowDropDownIcon /> : sort.priceChange1d === 'ascending' ? <ArrowDropUpIcon /> : ''}
+              </div>
+            </div>
+            {coinsData.map((item, index) => (
+              <Accordion key={item.id}>
+                <AccordionSummary onClick={(e) => handleClick(e, item.id)}>
+                  <div className='header'>
+                    <div className='rank'>{item.rank}</div>
+                    <div className='icon-wrapper'>
+                      <div className='icon-img-wrapper'><img src={item.icon} alt="item.name" /></div>
+                      <div>
+                        <div>{item.name}</div>
+                        <div className='symbol'>{item.symbol}</div>
+                      </div>
+                    </div>
+                    <div>{item.price}</div>
+                    <div className='market-cap'>{item.marketCap}</div>
+                    <div className='volume'>{item.volume}</div>
+                    <div className='supply'>{item.availableSupply}</div>
+                    <div className='change'>{item.priceChange1d}</div>
+                  </div>
+                </AccordionSummary>
+                <AccordionDetails>
+                  {markets[item.id] ?
+                    <TableContainer component={Paper}>
+                      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>Exchange</TableCell>
+                            <TableCell align="right">Pair</TableCell>
+                            <TableCell align="right">Pair Price</TableCell>
+                            <TableCell align="right">Price</TableCell>
+                            <TableCell align="right">Volume</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {markets[item.id].map((row, index) => (
+                            <TableRow
+                              key={index}
+                              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                              <TableCell component="th" scope="row">
+                                {row.exchange}
+                              </TableCell>
+                              <TableCell align="right">{row.pair}</TableCell>
+                              <TableCell align="right">{row.pairPrice}</TableCell>
+                              <TableCell align="right">{row.price}</TableCell>
+                              <TableCell align="right">{row.volume}</TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                    : <CircularProgress />}
+                </AccordionDetails>
+              </Accordion>
+            ))}
+          </div>)
+          :
+          pageLoading ? <CircularProgress /> :
+            <h1>No data to display</h1>}
+      </div>
     </div>
   );
 }
